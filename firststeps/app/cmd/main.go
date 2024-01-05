bbfa68fc9cfbcab102c41ca53209a6b89caff9a0
Alexandra Cañón Benavides <judy.canonbenavides@mercadolibre.com.mx>
2024-01-05 15:51:45 -0600

    first-steps: variables

---
+++ firststeps/app/cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	Name := "Alexandra"
+	Age := 29
+
+	fmt.Printf("My name is %s and I'm %d years old \n", Name, Age)
+
+	Blue := "Blue"
+	Pink := "Pink"
+	Black := "Black"
+
+	fmt.Printf("My favorite colors are %s, %s, %s \n", Blue, Pink, Black)
+
+}
